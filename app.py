bc61cb94bee3c6599f09960821d08f0bab747a8e
Wendy Vallejo <wbvalpat24@gmail.com>
2023-11-21 22:30:16 -0600

    update app streamlit

--- app.py
+++ app.py
@@ -5,31 +5,30 @@ import numpy as np
 
 def predict_linear_model(input_values):
     server_url = 'https://linear-model-service-wendyvallejo24.cloud.okteto.net/v1/models/linear-model:predict'
-    payload = {'instances': [[value] for value in input_values]}
-    print("Payload: ", payload)
 
+    payload = {'instances': [[value] for value in input_values]}
+    print("Payload:", payload)
     response = requests.post(server_url, data=json.dumps(payload))
     response.raise_for_status()
     prediction = response.json()
     return prediction
 
 def main():
-    st.title('Linear Model Prediction App')
+    st.title('Linear Model Prediction')
 
-    # Sidebar with input values
-    #st.sidebar.header('Input Values')
-    valor1 = st.number_input('Enter a value 1 to predict:', value=0.0)
-    valor2 = st.number_input('Enter a value 2 to predict:', value=0.0)
-    valor3 = st.number_input('Enter a value 3 to predict:', value=0.0)
+    # Input values
+    valor1 = st.number_input("Enter value 1 to predict: ", value=0.0)
+    valor2 = st.number_input("Enter value 2 to predict: ", value=0.0)
+    valor3 = st.number_input("Enter value 3 to predict: ", value=0.0)
 
-    if st.button('Predict'):
+    if st.button("Predict"):
         # Make prediction
-        valores = valor1, valor2, valor3
-        prediction = predict_linear_model([valores])
+        valores = valor1, valor2, valor2
+        prediction = predict_linear_model(valores)
 
-        st.success(f"Predicción: {np.argmax(prediction['predictions'])}")
+        # Display prediction
+        st.success(f"{prediction}")
 
-        st.write(f"Respuesta del API: {prediction}")
-    
+
 if __name__ == '__main__':
     main()
